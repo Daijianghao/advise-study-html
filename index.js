43c83516fddb5b657f3560c3f365fcc5e369f6e9
Huxin <70331183+Daijianghao@users.noreply.github.com>
2022-06-29 14:14:26 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         防颓脚本
+// @namespace    tim1103
+// @version      1.0
+// @description  避免颓废
+// @author       tim1103,huxin
+// @match        *://www.luogu.com.cn/discuss/*
+// @match        *://www.chess.com/
+// @match        *://www.lichess.org
+// @match        *://www.zhihu.com
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+//不许关掉，为期两月
+
+(function() {
+    'use strict';
+    location.href="https://daijianghao.github.io/advise-study-html/";
+})();
